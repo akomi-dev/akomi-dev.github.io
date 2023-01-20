9830cea09d99aa65eff4eb8812245cec10dc0653
Akomi <87880680+akomi-dev@users.noreply.github.com>
2023-01-20 01:19:45 -0500

    Initial Commit

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Freelancer", "Gamer", "Team Player"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+})
